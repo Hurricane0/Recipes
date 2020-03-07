04ef3d57cc4eedeca563fd409cb34fc005ce9a73
Hurricane0 <nikitadv777@gmail.com>
2020-03-07 21:40:06 +0200

    Updates

---
+++ precache-manifest.b8844c6a2b396fd4e0fba809869246f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0890e90337f2112dd8a3872f99ba5ae",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "765ee3522e66e8571f2c",
+    "url": "/Recipes/static/css/main.f572229a.chunk.css"
+  },
+  {
+    "revision": "5fa4de3ef6fac5b71d59",
+    "url": "/Recipes/static/js/2.70e813a2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Recipes/static/js/2.70e813a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765ee3522e66e8571f2c",
+    "url": "/Recipes/static/js/main.13c40f1c.chunk.js"
+  },
+  {
+    "revision": "e4a34bb9f22dba9b8e86",
+    "url": "/Recipes/static/js/runtime-main.0e9e9ab9.js"
+  }
+]);
